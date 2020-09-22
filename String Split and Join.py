57e51b7588c4a29199f749533b83eb368811e52c
a14guguliye <53395754+a14guguliye@users.noreply.github.com>
2020-09-22 19:58:11 +0400

    Add files via upload

---
+++ String Split and Join.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+inputString=input()
+
+
+# In[3]:
+
+
+inputStringSplittedList=inputString.split(" ");
+outputString="-".join(inputStringSplittedList)
+print(outputString)
+
+
+# In[ ]:
+
+
+
+
