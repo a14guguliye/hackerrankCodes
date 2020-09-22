5e750b2871e1985d7d06621e440f8deecb598248
a14guguliye <53395754+a14guguliye@users.noreply.github.com>
2020-09-22 20:27:14 +0400

    Add files via upload

---
+++ Whats Your Name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def print_full_name(a, b):
+    outputString="Hello "+a+" "+b+"! You just delved into python."
+    print(outputString)
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
+
+
+# In[ ]:
+
+
+
+
